9f573982feda39411f2b8def220b5f2716e9a367
kebin20 <ktanzyl@hotmail.com>
2022-12-15 15:42:57 +0900

    Completed mobile layout

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 // Import stylesheets
 import './style.css';
+
+/* Hamburger Menu Toggle */
+
+const hamburgerMenuButton = document.getElementById('menu-btn');
+const hamburgerMenu = document.getElementById('nav-links')
+
+function hamburgerMenuToggle() {
+  if (hamburgerMenu.style.display === 'block') {
+    hamburgerMenu.style.display = 'none';
+  } else {
+    hamburgerMenu.style.display = 'block';
+  }
+}
+
+hamburgerMenuButton.addEventListener('click', hamburgerMenuToggle);
